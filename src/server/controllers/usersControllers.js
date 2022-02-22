bbccc7cdf3aacc1ca6fe858f16e11cb9d5182089
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-22 13:45:50 +0100

    added get function to check on username and give token

--- src/server/controllers/usersControllers.js
+++ src/server/controllers/usersControllers.js
@@ -1,5 +1,31 @@
+const jsonwebtoken = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../../database/models/User");
+
 const getLogin = async (req, res, next) => {
   const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    const error = new Error("Sorry, you are not invited to the party");
+    error.code = 404;
+    next(error);
+  } else {
+    const userData = {
+      username: user.name,
+      id: user.id,
+    };
+    const rightPassword = await bcrypt.compare(password, user.password);
+    if (!rightPassword) {
+      const error = new Error("Sorry, you are not who you say you are");
+      error.code = 403;
+      next(error);
+    } else {
+      const token = jsonwebtoken.sign(userData, process.env.SECRET);
+      res.json({ token });
+    }
+  }
 };
 
 module.exports = { getLogin };
