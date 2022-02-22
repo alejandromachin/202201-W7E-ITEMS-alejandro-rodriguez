550184302464ece9e808aace3c9ab2bcbcc19540
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-22 15:12:56 +0100

    validated a encrypted password

--- src/server/controllers/usersControllers.js
+++ src/server/controllers/usersControllers.js
@@ -1,6 +1,7 @@
 const jsonwebtoken = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../../database/models/User");
+const encryptPassword = require("../utils/encryptPassword");
 
 const getLogin = async (req, res, next) => {
   const { username, password } = req.body;
@@ -30,9 +31,20 @@ const getLogin = async (req, res, next) => {
 
 const registerUser = async (req, res, next) => {
   const user = req.body;
+  const { username, password } = user;
 
-  await User.create(user);
-  res.json(user);
+  const existingUser = await User.findOne({ username });
+
+  if (!existingUser) {
+    const encryptedPassword = await encryptPassword(password);
+    user.password = encryptedPassword;
+    const createdUser = await User.create(user);
+    res.json(createdUser);
+  } else {
+    const error = new Error("Sorry, username alredy taken");
+    error.code = 409;
+    next(error);
+  }
 };
 
 module.exports = { getLogin, registerUser };
